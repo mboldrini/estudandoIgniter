5effaec87e04a0d085cc310af449f8b1a342ca55
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2017-05-11 14:24:58 -0300

    resolvi uns bugs
    codigo que esqueci de colocar alem da parte do painel

--- application/controllers/Painel.php
+++ application/controllers/Painel.php
@@ -50,10 +50,16 @@ class Painel extends CI_Controller {
 			redirect(base_url().'login');
 		}
 
+		# pega o nome do usuario que tem na session e passa >
+		$nome = $this->session->userdata('username');
+		# pega o nome da variavel aqui de cima, e faz uma pesquisa completa no banco de dados 'user'
+		$pegaInfos = $this->usuario->pegaUsuario($nome);
+
 		$dados = array(
 			'tela' => 'cliente',
 			'titulo' => 'Clientes',
-			'descricao' => ' - Cadastro de clientes para execução de serviços'
+			'descricao' => ' - Cadastro de clientes para execução de serviços',
+			'infos' => $pegaInfos,
 		);
 
 		$this->load->view('cliente',$dados);
@@ -67,10 +73,16 @@ class Painel extends CI_Controller {
 			redirect(base_url().'login');
 		}
 
+		# pega o nome do usuario que tem na session e passa >
+		$nome = $this->session->userdata('username');
+		# pega o nome da variavel aqui de cima, e faz uma pesquisa completa no banco de dados 'user'
+		$pegaInfos = $this->usuario->pegaUsuario($nome);
+
 		$dados = array(
 			'tela' => 'tiposervico',
 			'titulo' => 'Tipos de Serviços',
-			'descricao' => ' - Cadastro de tipos de serviços'
+			'descricao' => ' - Cadastro de tipos de serviços',
+			'infos' => $pegaInfos,
 		);
 
 		$this->load->view('tiposervico',$dados);
@@ -82,8 +94,14 @@ class Painel extends CI_Controller {
 		$dados = array(
 			'tela' => 'valorservico',
 			'titulo' => 'Valores de Serviços',
-			'descricao' => ' - Cadastro de valores de serviços'
+			'descricao' => ' - Cadastro de valores de serviços',
+			'infos' => $pegaInfos,
 		);
+
+		# pega o nome do usuario que tem na session e passa >
+		$nome = $this->session->userdata('username');
+		# pega o nome da variavel aqui de cima, e faz uma pesquisa completa no banco de dados 'user'
+		$pegaInfos = $this->usuario->pegaUsuario($nome);
 
 		$this->load->view('valorservico',$dados);
 
@@ -95,8 +113,14 @@ class Painel extends CI_Controller {
 		$dados = array(
 			'tela' => 'servico',
 			'titulo' => 'Serviços',
-			'descricao' => ' - Cadastro de Serviços'
+			'descricao' => ' - Cadastro de Serviços',
+			'infos' => $pegaInfos,
 		);
+
+		# pega o nome do usuario que tem na session e passa >
+		$nome = $this->session->userdata('username');
+		# pega o nome da variavel aqui de cima, e faz uma pesquisa completa no banco de dados 'user'
+		$pegaInfos = $this->usuario->pegaUsuario($nome);
 
 		$this->load->view('servico',$dados);
 
